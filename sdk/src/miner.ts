45ad8d61fe755013c850d6fcd546866e7222a93e
pycckuu <is.markelov@gmail.com>
2023-07-03 15:19:09 +0200

    fix: update license and readme files

--- sdk/src/miner.ts
+++ sdk/src/miner.ts
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: BUSL-1.1
+// SPDX-FileCopyrightText: Copyright 2021-22 Panther Ventures Limited Gibraltar
+
 import {BusTree} from 'contract/bus-tree-types';
 import {Contract, ContractReceipt, Wallet, getDefaultProvider} from 'ethers';
 
