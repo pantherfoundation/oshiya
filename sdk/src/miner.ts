e4e96e922e233d2843f5e499e95858c7c153855c
pycckuu <is.markelov@gmail.com>
2023-07-05 13:01:08 +0200

    feat(miner): generate random UTXOs and add them to the BusQueue
    This commit introduces a new function to generate a random UTXOs within
    the Snark Field. It also updates the Miner class to simulate the
    addition of random UTXOs to the BusQueue.
    
    Changes include:
    
    - Implemented getRandomInSnarkField function to generate a random UTXO
      in the range of the Snark Field. The function generates a random
      256-bit number and converts it to a 32 bytes string, ensuring the
      value stays within the range of the Snark Field.
    
    - Added simulateAddUtxosToBusQueue method in the Miner class. This
      function generates a random number of UTXOs (between 1 and 8) using
      the getRandomInSnarkField function, and then simulates the addition of
      these UTXOs to the BusQueue in the smart contract.

--- sdk/src/miner.ts
+++ sdk/src/miner.ts
@@ -3,10 +3,15 @@
 
 import {ContractReceipt, Wallet} from 'ethers';
 
+import {bigintToBytes32} from './bigint-conversions';
 import {BusTree} from './contract/bus-tree-types';
 import {initializeBusContract} from './contracts';
 import {LogFn, log as defaultLog} from './logging';
 import {BusQueueRecStructOutput} from './types';
+
+const SNARK_FIELD_SIZE = BigInt(
+    '21888242871839275222246405745257275088548364400416034343698204186575808495617',
+);
 
 function selectHighestN<T>(
     array: Array<T>,
@@ -30,6 +35,19 @@ function selectHighestN<T>(
 function getRandomElement<T>(array: Array<T>): T {
     const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
+}
+
+function getRandomInSnarkField(): string {
+    const bytes = 32;
+    let hex = '';
+    for (let i = 0; i < bytes; i++) {
+        // Generate a random byte (0-255)
+        const randomByte = Math.floor(Math.random() * 256);
+        // Convert the byte to hexadecimal (always results in a 2-character string)
+        const hexByte = randomByte.toString(16).padStart(2, '0');
+        hex += hexByte;
+    }
+    return bigintToBytes32(BigInt(`0x${hex}`) % SNARK_FIELD_SIZE);
 }
 
 export class Miner {
@@ -78,6 +96,23 @@ export class Miner {
         );
         this.log(`Submitted tx ${tx.hash}`);
         return await tx.wait();
+    }
+
+    public async simulateAddUtxosToBusQueue(): Promise<ContractReceipt> {
+        const numUtxos = Math.floor(Math.random() * 8) + 1;
+        const utxos: Array<string> = [];
+        for (let i = 0; i < numUtxos; i++) {
+            utxos.push(getRandomInSnarkField());
+        }
+        const tx = await this.busContract.simulateAddUtxosToBusQueue(
+            utxos,
+            numUtxos * 10,
+        );
+        this.log(
+            `Submitted tx ${tx.hash} for generating ${numUtxos} random utxos`,
+        );
+
+        return tx.wait();
     }
 
     public async getBusTreeRoot(): Promise<string> {
