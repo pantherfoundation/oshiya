e1d31401fc5df140ec5abb433a64c9af2b0b3d15
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-07-06 17:27:11 +0300

    feat: render error screen for missing required env vars

--- web-client/src/services/env.ts
+++ web-client/src/services/env.ts
@@ -4,4 +4,12 @@ export const env = Object.freeze({
     RPC_URL: process.env.RPC_URL,
     SUBGRAPH_ID: process.env.SUBGRAPH_ID,
     ZKP_TOKEN_ADDRESS: process.env.ZKP_TOKEN_ADDRESS,
+    GENESIS_BLOCK_NUMBER: process.env.GENESIS_BLOCK_NUMBER,
 });
+
+export const requiredEnvVars: Array<keyof typeof env> = [
+    'CONTRACT_ADDRESS',
+    'SUBGRAPH_ID',
+    'GENESIS_BLOCK_NUMBER',
+    'ZKP_TOKEN_ADDRESS',
+];
