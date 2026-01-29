08fb2292e9a1d99ce4b637d93d799a8473a20500
Ahmed Ghait <me@ahmedibrahim.dev>
2026-01-29 12:40:34 +0200

    feat: add updated circuits files

--- sdk/src/zk-prover.ts
+++ sdk/src/zk-prover.ts
@@ -7,29 +7,29 @@ import {ProofInputs} from './types';
 import verificationKey from './wasm/verificationKey.json';
 
 export class ZKProver {
-    private readonly verificationKey: any;
+  private readonly verificationKey: any;
 
-    constructor(
-        private readonly wasmFilePath: string,
-        private readonly zKeyPath: string,
-    ) {
-        this.verificationKey = verificationKey;
-    }
+  constructor(
+    private readonly wasmFilePath: string,
+    private readonly zKeyPath: string,
+  ) {
+    this.verificationKey = verificationKey;
+  }
 
-    public async generateProof(pInput: ProofInputs): Promise<any> {
-        const {proof: lProof, publicSignals} = await groth16.fullProve(
-            pInput,
-            this.wasmFilePath,
-            this.zKeyPath,
-            null,
-        );
+  public async generateProof(pInput: ProofInputs): Promise<any> {
+    const {proof: lProof, publicSignals} = await groth16.fullProve(
+      pInput,
+      this.wasmFilePath,
+      this.zKeyPath,
+      null,
+    );
 
-        const solProof = (replica => [
-            replica.pi_a.slice(0, 2),
-            replica.pi_b.slice(0, 2).map((x: any) => x.reverse()),
-            replica.pi_c.slice(0, 2),
-        ])(JSON.parse(JSON.stringify(lProof)));
+    const solProof = (replica => [
+      replica.pi_a.slice(0, 2),
+      replica.pi_b.slice(0, 2).map((x: any) => x.reverse()),
+      replica.pi_c.slice(0, 2),
+    ])(JSON.parse(JSON.stringify(lProof)));
 
-        return [solProof, publicSignals];
-    }
+    return [solProof, publicSignals];
+  }
 }
